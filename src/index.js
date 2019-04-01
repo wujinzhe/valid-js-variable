07aace8b8f6b23edf20bd7dd5af7c2a8795d5edd
wujinzhe <1037680926@qq.com>
2019-04-01 23:15:15 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const  unicode_id_start_regex = require('unicode-12.0.0/Binary_Property/ID_Start/regex.js')
+const  unicode_id_continue_regex = require('unicode-12.0.0/Binary_Property/ID_Continue/regex.js')
+
+console.log('start', unicode_id_start_regex.test('我'))
+// console.log('continue', unicode_id_continue.indexOf('$'))
+
