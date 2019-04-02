19dce947d0e5c7c998b5f4f45aa699a63b67f605
wujinzhe <1037680926@qq.com>
2019-04-03 07:27:43 +0800

    修改webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 module.exports = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'vjv.min.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+
 }
